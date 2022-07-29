35a81be177764d744b399bf39af641de6dd667ab
Cris-Bento <bentocristiane1@gmail.com>
2022-07-28 22:08:34 -0300

    corrigindo

--- src/bancoDeDados/conexao.js
+++ src/bancoDeDados/conexao.js
@@ -1,21 +1,24 @@
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: { rejectUnauthorized: false }
+//     }
+// });
+
+module.exports = knex;
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: 5432,
         ssl: { rejectUnauthorized: false }
+    
     }
 });
 
 module.exports = knex;
-
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_NAME
-//     }
-// });
-
-// module.exports = knex;
